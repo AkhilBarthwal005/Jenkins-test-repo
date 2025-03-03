05fba813f52d6b36905b762e39cd677cf21a273d
Akhil Barthwal <67703904+AkhilBarthwal005@users.noreply.github.com>
2025-03-03 15:34:21 +0530

    Update Hello_word.java

--- Hello_word.java
+++ Hello_word.java
@@ -2,6 +2,6 @@ package com.example.DeliveryBoy.service;
 
 public class Hello_word {
     public static void main(String[] args) {
-        System.out.println("Hello-word");
+        System.out.println("Hello-word with me");
     }
 }
